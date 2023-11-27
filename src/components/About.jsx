6e589b94da5d4e0914cc9f099746b3f04b65e5b8
fodie Niakate <fodieniak4@gmail.com>
2023-11-27 17:04:25 +0100

    modif page about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,24 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 import '../styles/About.css'
-
-const Collapsible = ({ title, content }) => {
-  const [isOpen, setIsOpen] = useState(false)
-
-  return (
-    <div className="collapsible">
-      <button onClick={() => setIsOpen(!isOpen)}>{title}</button>
-      {isOpen && <div className="content">{content}</div>}
-    </div>
-  )
-}
+import Collapse from './Collapse'
 
 const About = () => {
   return (
     <div className="About">
-      <Collapsible title="Flabilité" content="Contenu sur la flabilité." />
-      <Collapsible title="Respect" content="Contenu sur le respect." />
-      <Collapsible title="Service" content="Contenu sur le service." />
-      <Collapsible title="Sécurité" content="Contenu sur la sécurité." />
+      <Collapse title="Flabilité" content="Contenu sur la flabilité." />
+      <Collapse title="Respect" content="Contenu sur le respect." />
+      <Collapse title="Service" content="Contenu sur le service." />
+      <Collapse title="Sécurité" content="Contenu sur la sécurité." />
     </div>
   )
 }
