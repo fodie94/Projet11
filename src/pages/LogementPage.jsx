57d15393b82f422ecf6b66b8d92688e12f2ec47c
fodie Niakate <fodieniak4@gmail.com>
2023-12-07 18:04:02 +0100

    ajout error page pour logement

--- src/pages/LogementPage.jsx
+++ src/pages/LogementPage.jsx
@@ -4,7 +4,7 @@ import { useParams } from 'react-router-dom'
 import logements from '../data/logements.json'
 import Logement from '../components/Logement.jsx'
 // import '../styles/root.css'
-
+import ErrorPage from './ErrorPage'
 export default function LogementPage() {
   const { id } = useParams()
   console.log("ID extrait de l'URL :", id)
@@ -14,7 +14,7 @@ export default function LogementPage() {
   if (!logement) {
     // Gérer le cas où aucun logement n'est trouvé pour l'ID spécifié
     console.log('Logement non trouvé')
-    return <p>Logement non trouvé</p>
+    return <ErrorPage />
   }
 
   return <Logement key={logement.id} {...logement} />
